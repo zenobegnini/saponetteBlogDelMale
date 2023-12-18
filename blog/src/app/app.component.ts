c863f3257ae7e0daeeabc25fbf742ce277850e52
gab03gab <gabrielnori03@gmail.com>
2023-12-18 11:56:41 +0100

    .

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { ButtonModule } from 'primeng/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidebarComponent, ButtonModule],
+  imports: [CommonModule, RouterOutlet, ButtonModule, SidebarModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'blog';
+
+  public sidebarVisible: boolean = false;
 }
--- blog/src/app/home/home.component.ts
+++ blog/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SidebarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
