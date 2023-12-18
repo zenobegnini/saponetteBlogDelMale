616d1cbb80c5f4de118568f3d8ab346a85b2a138
ferdinando02 <contiferdinando02@gmail.com>
2023-12-18 10:59:25 +0100

    remove MenubarModule

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MenubarModule } from 'primeng/menubar';
+
 @Component({
   selector: 'app-root',
   standalone: true,
