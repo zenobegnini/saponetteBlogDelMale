23677c4f0b7ea11e218fc9a757a5e105a9324f23
gab03gab <gabrielnori03@gmail.com>
2023-12-18 10:51:52 +0100

    [ADD] routes

--- blog/src/app/app.routes.ts
+++ blog/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent,
+        
+
+    },
+    {
+        path: 'home',
+        pathMatch: 'full',
+        loadComponent: () =>
+          import('./home/home.component').then((c) => c.HomeComponent),
+    },
+    
+];
